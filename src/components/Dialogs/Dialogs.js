5f2ba5dff6c6a0d0023a785bbccd4c94cee46eee
emoface1337 <chvrches@mail.ru>
2020-09-22 19:25:15 +0300

    added simple layout for dialogs page

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import './Dialogs.sass'
+
 const Dialogs = () => {
     return (
         <div className="content__dialogs">
-            Диалоги
+            <div className="dialogs__users">
+                <div className="dialogs__user active">
+                    Яна
+                </div>
+                <div className="dialogs__user">
+                    Аня
+                </div>
+            </div>
+            <div className="dialogs__messages">
+                <div className="dialogs__message">Сообщение 1</div>
+                <div className="dialogs__message">Сообщение 1</div>
+                <div className="dialogs__message">Сообщение 1</div>
+            </div>
         </div>
     )
 }
