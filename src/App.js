45a177898eafe2ef3a869e76cb815d13308a11f6
emoface1337 <chvrches@mail.ru>
2020-09-22 19:08:42 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+
 import './App.sass'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 import Dialogs from './components/Dialogs/Dialogs'
 
+
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <section className="content">
-                {/*<Dialogs/>*/}
-                <Profile/>
-            </section>
-
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <section className="content">
+                    <Route path='/profile' exact component={Profile}/>
+                    <Route path='/dialogs' exact component={Dialogs}/>
+                </section>
+            </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import './Navbar.sass'
 
 const Navbar = () => {
     return (
         <nav className="nav">
             <ul className="nav__list">
-                <li className="nav__list-link"><a href="/profile">Профиль</a></li>
-                <li className="nav__list-link"><a href="/messages">Сообщения</a></li>
-                <li className="nav__list-link"><a href="/news">Новости</a></li>
-                <li className="nav__list-link"><a href="/music">Музыка</a></li>
-                <li className="nav__list-link"><a href="/settings">Настройки</a></li>
+                <li className="nav__list-link"><NavLink activeClassName="active" to="/profile">Профиль</NavLink></li>
+                <li className="nav__list-link"><NavLink activeClassName="active" to="/dialogs">Сообщения</NavLink></li>
+                <li className="nav__list-link"><NavLink activeClassName="active" to="/news">Новости</NavLink></li>
+                <li className="nav__list-link"><NavLink activeClassName="active" to="/music">Музыка</NavLink></li>
+                <li className="nav__list-link"><NavLink activeClassName="active" to="/settings">Настройки</NavLink></li>
             </ul>
         </nav>
     )
